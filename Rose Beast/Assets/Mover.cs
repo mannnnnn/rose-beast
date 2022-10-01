66c51bf06aed9aa92c9fd3fa8fdb356d8792398d
Savannah Mann <mannnnnn@users.noreply.github.com>
2022-09-30 20:13:12 -0500

    Mover added
    Objects with the Mover component can move across the grid. The one on the player uses player input

---
+++ Rose Beast/Assets/Mover.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Mover : MonoBehaviour {
+
+    private Tilemap tilemap;
+    private bool isMoving = false;
+    float movementDelay = 0.1f;
+
+    public void Start(){
+        tilemap = FindObjectOfType<Tilemap>();
+    }
+
+    public void Move(Vector2 dir){
+        if(CanMove(dir) && !isMoving){
+            StartCoroutine(Moving(dir));
+        } else if(!CanMove(dir)){
+            StartCoroutine(Blocked(dir));
+        }
+    }
+
+    public bool CanMove(Vector2 dir){
+        return true;
+    }
+
+    IEnumerator Moving(Vector2 dir)
+    {
+       isMoving = true;
+       float origZ = transform.position.z;
+       Vector3Int origPosition = tilemap.WorldToCell(transform.position);
+
+       Vector3Int nextCell = tilemap.WorldToCell((Vector2)transform.position) + new Vector3Int((int)dir.x, (int)dir.y, 0);
+
+       float elapsedTime = 0;
+        while(elapsedTime < movementDelay){
+            transform.position = Vector2.Lerp(tilemap.GetCellCenterWorld(origPosition), tilemap.GetCellCenterWorld(nextCell), elapsedTime/movementDelay);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+      transform.position = tilemap.GetCellCenterWorld(nextCell);
+       isMoving = false;
+       yield return null;
+    }
+
+    IEnumerator Blocked(Vector2 dir)
+    {
+       isMoving = true;
+       float origZ = transform.position.z;
+       Vector3Int origPosition = tilemap.WorldToCell(transform.position);
+       Vector3Int nextCell = tilemap.WorldToCell((Vector2)transform.position) + new Vector3Int((int)dir.x, (int)dir.y, 0);
+
+       float elapsedTime = 0;
+        while(elapsedTime < movementDelay/2){
+            transform.position = Vector2.Lerp(tilemap.GetCellCenterWorld(origPosition), tilemap.GetCellCenterWorld(nextCell), elapsedTime/movementDelay/2);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Vector3 halfPos = transform.position;
+         while(elapsedTime < movementDelay){
+            transform.position = Vector2.Lerp(halfPos, tilemap.GetCellCenterWorld(origPosition), elapsedTime/movementDelay/2);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+      transform.position = tilemap.GetCellCenterWorld(origPosition);
+       isMoving = false;
+       yield return null;
+    }
+
+
+}
---
+++ Rose Beast/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Mover mover;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    void OnMove(InputValue input){
+        moveInput = input.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(moveInput.magnitude > 0f) {
+            if(Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y)){
+                if(moveInput.x > 0){
+                    mover.Move(Vector2.right);
+                } else if (moveInput.x < 0){
+                    mover.Move(Vector2.left);
+                }
+            } else{
+                 if(moveInput.y > 0){
+                    mover.Move(Vector2.up);
+                } else if (moveInput.y < 0){
+                    mover.Move(Vector2.down);
+                }
+            }
+        }
+    }
+}
